54f7e4877deb9c821144ca8384f61d50b48d79ca
Satvik Tiwari <satviktiwarikivtas7@gmail.com>
2021-12-06 17:18:41 +0530

    initial commit

---
+++ Card crafting Game/Assets/GameSession.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI levelNumberText;
+    [SerializeField] bool clearPlayerPrefs = false;
+
+    LevelLoader levelLoader;
+    int currLevel;
+    //[SerializeField] GameObject continueButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(clearPlayerPrefs)
+            PlayerPrefs.DeleteAll();
+        currLevel = PlayerPrefs.GetInt("Level", 1);
+        levelNumberText.text = currLevel.ToString();
+        levelLoader = FindObjectOfType<LevelLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Continue()
+    {
+        currLevel++;
+        PlayerPrefs.SetInt("Level", currLevel);
+        levelLoader.LoadNextScene();
+    }
+
+}
---
+++ Card crafting Game/Assets/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        int idx = SceneManager.GetActiveScene().buildIndex + 1;
+        if (idx >= SceneManager.sceneCountInBuildSettings)
+        {
+            idx = 0;
+        }
+        LoadScene(idx);
+    }
+
+    private void LoadScene(int index)
+    {
+        //PlayerPrefs.SetInt("Level", index+1);
+        SceneManager.LoadScene(index);
+    }
+}
